3b58f5ff78fae01669f9b86d7ed0528b81b1a482
Fatih Kürekçi <fatihkr02@gmail.com>
2024-12-05 02:06:43 +0300

    articles listed on home page

---
+++ PresentationLayer/ViewComponents/_DefaultArticleListComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.ViewComponents
+{
+    public class _DefaultArticleListComponentPartial : ViewComponent
+    {
+        private readonly IArticleService _articleService;
+
+        public _DefaultArticleListComponentPartial(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var articles = _articleService.TApprovedArticles();
+            return View(articles);
+        }
+    }
+}
