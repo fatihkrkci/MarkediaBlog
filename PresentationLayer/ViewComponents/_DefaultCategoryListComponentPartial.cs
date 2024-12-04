32263434d593ae6e391836c8cbae23e6dfce516f
Fatih Kürekçi <fatihkr02@gmail.com>
2024-12-05 01:57:56 +0300

    added default website template & categories listed on home page

---
+++ PresentationLayer/ViewComponents/_DefaultCategoryListComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.ViewComponents
+{
+    public class _DefaultCategoryListComponentPartial : ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+
+        public _DefaultCategoryListComponentPartial(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _categoryService.TGetActiveCategoriesSortedDescending();
+            return View(values);
+        }
+    }
+}
